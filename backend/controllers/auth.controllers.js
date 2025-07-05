4a19e33726fb444a59abb45f773e3400e46ff244
Yash Vardhan Singh <singhyashvardhan34@gmail.com>
2025-07-05 16:39:50 +0530

    initial commit

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -51,12 +51,13 @@ try {
 
     const token=await genToken(user._id)
 
-    res.cookie("token",token,{
-        httpOnly:true,
-       maxAge:7*24*60*60*1000,
-       sameSite:"strict",
-       secure:false
-    })
+   res.cookie("token", token, {
+     httpOnly: true,
+     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+     sameSite: "None",               // ✅ Needed for cross-site cookie
+     secure: true                    // ✅ Required if sameSite: 'None'
+   });
+
 
     return res.status(200).json(user)
 
